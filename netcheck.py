776f776d54582ff7865d8d3217ad4ad14f238296
Joseph Redfern <joseph@redfern.me>
2018-10-10 20:30:52 +0100

    OO-ified!

--- netcheck.py
+++ netcheck.py
@@ -4,11 +4,10 @@ Description   :: A tool for automated ping and speedtest scheduling, configurabl
 Project Home  :: https://github.com/luketimothyjones/pynetcheck
 License       :: MIT + attribution (maintaining this header block is plenty)
 Contributors  :: Luke Pflibsen-Jones [GH: luketimothyjones] (author)
-                 
+
 """
 
 import csv
-import os
 import re
 import sqlite3
 import subprocess
@@ -19,156 +18,180 @@ import arrow
 import speedtest
 
 
-# ------
-DELAY_MINUTES = 10
-
 TIMEZONE = 'US/Pacific'
 TIMESTAMP_FORMAT = 'YY/MM/DD HH:mm'
 
-PING_DESTINATION = 'www.google.com'
-PING_QUANTITY = 25
+
+class PyNetCheck:
+    def __init__(self, ping_count, ping_host, test_delay, db_filename, timezone, timestamp_format):
+        self.ping_count = ping_count
+        self.ping_host = ping_host
+        self.test_delay = test_delay
+        self.db = sqlite3.connect(db_filename)
+        self.timezone = timezone
+        self.timestamp_format = timestamp_format
+
+    def maybe_create_tables(self):
+        """
+        Creates database tables if they do not already exist
+        """
+
+        pings = '''
+        CREATE TABLE IF NOT EXISTS `pings` (
+        `date`	VARCHAR(20),
+        `percent_lost`	INTEGER,
+        `packets_sent`	INTEGER,
+        `min_ms`	INTEGER,
+        `max_ms`	INTEGER,
+        `average_ms`	INTEGER,
+        PRIMARY KEY(date));
+        '''
+
+        speedtests = '''
+        CREATE TABLE IF NOT EXISTS `speedtests` (
+        `date`	VARCHAR(20),
+        `ping`	INTEGER,
+        `download_mbps`	FLOAT,
+        `upload_mbps`	FLOAT,
+        `server`	VARCHAR(100),
+        PRIMARY KEY(date));
+        '''
+
+        self.db.execute(pings)
+        self.db.execute(speedtests)
+
+    def consprint(self, string='', end='\n'):
+        """
+        Helper method to print to console, allowing previous line to be over-written.
+        """
+        sys.stdout.write(string + end)
+        sys.stdout.flush()
+
+    def ping_speedtest_save(self):
+        """
+        Run single ping test, speed test, and write results to DB.
+        """
+
+        self.consprint('Running pings...', end='')
+
+        datetime = arrow.now(TIMEZONE).format(TIMESTAMP_FORMAT)
+
+        percent_lost, min_ms, average_ms, max_ms = self.execute_ping()
+
+        self.consprint('\rRunning speedtest...', end='')
+
+        sptest = speedtest.Speedtest()
+        sptest.get_best_server()
+
+        speedtest_ping = int(sptest.results.ping)
+        speedtest_dl_mbps = round(sptest.download() / 1000000, 2)  # Convert from bits/s to megabits/s
+        speedtest_up_mbps = round(sptest.upload() / 1000000, 2)
+
+        speedtest_server = f'{sptest.results.server["sponsor"]} ({sptest.results.server["name"]})'
+
+        self.consprint(f'\r{datetime}   {percent_lost:<7}{min_ms:<7}{max_ms:<7}{average_ms:<7}|    '
+                f'{speedtest_ping:<7}{"{:.2f}".format(speedtest_dl_mbps):<11}{"{:.2f}".format(speedtest_up_mbps):<11}{speedtest_server}')
+
+        # Save results to database
+        with self.db:
+            self.db.execute("""
+                            INSERT INTO pings(date, percent_lost, packets_sent, min_ms, max_ms, average_ms)
+                            VALUES (?, ?, ?, ?, ?, ?)
+                            """,
+                            (datetime, percent_lost, self.ping_count, min_ms, max_ms, average_ms))
+
+            self.db.execute("""
+                            INSERT INTO speedtests(date, ping, download_mbps, upload_mbps, server)
+                            VALUES (?, ?, ?, ?, ?)
+                            """,
+                            (datetime, speedtest_ping, speedtest_dl_mbps, speedtest_up_mbps, speedtest_server))
+
+    def execute_ping(self, host=None, count=None):
+        """
+        Cross-platform ping implementation. Only tested under linux, but should be
+        functional under macOS and Cygwin as AFAIK their outputs are the same...
+        """
+
+        host = host or self.ping_host
+        count = count or self.ping_count
+
+        if sys.platform in ('linux', 'cygwin', 'darwin'):
+            percent_lost_re = re.compile(r'(?P<percent_lost>\d*[.,]?\d*)% packet loss')
+            min_max_avg_re = re.compile(r'min/avg/max/mdev = (?P<min>\d*[.,]?\d*)/(?P<avg>\d*[.,]?\d*)/(?P<max>\d*[.,]?\d*)')
+
+            data = str(subprocess.Popen(['ping', host, '-c', str(count)], stdout=subprocess.PIPE).stdout.read())
+        elif sys.platform == 'win32':
+            percent_lost_re = re.compile(r'(?<percent_lost>[0-9]{1,3})% loss')
+            min_max_avg_re = re.compile(r'Minimum = (?P<min>\d+)ms, Maximum = (?P<max>\d+)ms, Average = (?P<avg>\d+)ms')
+
+            data = str(subprocess.Popen(['ping', host, '-n', str(count)], stdout=subprocess.PIPE).stdout.read())[2:-1]
+        else:
+            raise Exception('Unsupported Platform.')
+
+        percent_lost_match = percent_lost_re.search(data)
+        timing_match = min_max_avg_re.search(data)
+
+        percent_lost = int(round(float(percent_lost_match.group('percent_lost'))))
+        min_ms = int(round(float(timing_match.group('min'))))
+        average_ms = int(round(float(timing_match.group('avg'))))
+        max_ms = int(round(float(timing_match.group('max'))))
+
+        return percent_lost, min_ms, average_ms, max_ms
+
+    def dump_data_to_csv(self):
+        csv.register_dialect('pretty', delimiter='\t', quoting=csv.QUOTE_NONE)
+
+        with self.db:
+            with open('pings.csv', 'w', newline='') as ping_file:
+                print('\t'.join('Date/time Percent_Lost Packets_Sent Min Max Avg'.split()), file=ping_file)
+
+                ping_writer = csv.writer(ping_file, dialect='pretty')
+                ping_data = self.db.execute('SELECT * FROM pings').fetchall()
+                ping_writer.writerows(ping_data)
+
+            with open('speedtests.csv', 'w', newline='') as speedtest_file:
+                print('\t'.join('Date/time Ping Download Upload Server'.split()), file=speedtest_file)
+
+                speedtest_writer = csv.writer(speedtest_file, dialect='pretty')
+                speedtest_data = self.db.execute('SELECT * FROM speedtests').fetchall()
+                speedtest_writer.writerows(speedtest_data)
+
+    def run(self):
+        """
+        Create tables (if needed), and start running the test loop.
+        """
+        self.maybe_create_tables()
+        self.consprint('Date/time        Lost   Min    Max    Avg    |    Ping   Download   Upload     Server')
+
+        while True:
+            self.ping_speedtest_save()
+            time.sleep(self.test_delay * 60)
 
 
-database_connection = sqlite3.connect('connection_data.sqlite')
-
-
-def maybe_create_tables():
-    """
-    Creates database tables if they do not already exist
-    """
-
-    pings = \
-'''CREATE TABLE `pings` (
-	`date`	VARCHAR(20),
-	`percent_lost`	INTEGER,
-	`packets_sent`	INTEGER,
-	`min_ms`	INTEGER,
-	`max_ms`	INTEGER,
-	`average_ms`	INTEGER,
-	PRIMARY KEY(date)
-);'''
-
-    speedtests = \
-'''CREATE TABLE `speedtests` (
-	`date`	VARCHAR(20),
-	`ping`	INTEGER,
-	`download_mbps`	FLOAT,
-	`upload_mbps`	FLOAT,
-	`server`	VARCHAR(100),
-	PRIMARY KEY(date)
-);'''
-
-    pings_checker = "SELECT name FROM sqlite_master WHERE type='table' AND name='pings';"
-    speedtests_checker = "SELECT name FROM sqlite_master WHERE type='table' AND name='speedtests';"
-
-    # Make sure the tables exist in the database
-    with database_connection:
-        if database_connection.execute(pings_checker).fetchone() is None and \
-           database_connection.execute(speedtests_checker).fetchone() is None:
-            database_connection.execute(pings)
-            database_connection.execute(speedtests)
-
-# ------
-def consprint(string='', end='\n'):
-    sys.stdout.write(string + end)
-    sys.stdout.flush()
-
-# ------
-def ping_speedtest_save():
-    consprint('Running pings...', end='')
-
-    datetime = arrow.now(TIMEZONE).format(TIMESTAMP_FORMAT)
-
-    percent_lost, min_ms, average_ms, max_ms = ping(PING_DESTINATION, PING_QUANTITY)
-
-    # ---
-    consprint('\rRunning speedtest...', end='')
-
-    sptest = speedtest.Speedtest()
-    sptest.get_best_server()
-
-    speedtest_ping = int(sptest.results.ping)
-    speedtest_dl_mbps = round(sptest.download() / 1000000, 2)  # Convert from bits/s to megabits/s
-    speedtest_up_mbps = round(sptest.upload() / 1000000, 2)
-
-    speedtest_server = f'{sptest.results.server["sponsor"]} ({sptest.results.server["name"]})'
-
-    consprint(f'\r{datetime}   {percent_lost:<7}{min_ms:<7}{max_ms:<7}{average_ms:<7}|    '
-              f'{speedtest_ping:<7}{"{:.2f}".format(speedtest_dl_mbps):<11}{"{:.2f}".format(speedtest_up_mbps):<11}{speedtest_server}')
-
-    # ---
-    # Save results to database
-    with database_connection:
-        database_connection.execute(
-            'INSERT INTO pings(date, percent_lost, packets_sent, min_ms, max_ms, average_ms) VALUES (?, ?, ?, ?, ?, ?)',
-            (datetime, percent_lost, PING_QUANTITY, min_ms, max_ms, average_ms))
-
-        database_connection.execute(
-            'INSERT INTO speedtests(date, ping, download_mbps, upload_mbps, server) VALUES (?, ?, ?, ?, ?)',
-            (datetime, speedtest_ping, speedtest_dl_mbps, speedtest_up_mbps, speedtest_server))
-
-def ping(host, count):
-    """
-    Cross-platform ping implementation. Only tested under linux, but should be
-    functional under macOS and Cygwin as AFAIK their outputs are the same...
-    """
-    if sys.platform in ('linux', 'cygwin', 'darwin'):
-        percent_lost_re = re.compile('(?P<percent_lost>\d*[.,]?\d*)% packet loss')
-        min_max_avg_re = re.compile("min/avg/max/mdev = (?P<min>\d*[.,]?\d*)/(?P<avg>\d*[.,]?\d*)/(?P<max>\d*[.,]?\d*)")
-
-        data = str(subprocess.Popen(['ping', host, '-c', str(count)], stdout=subprocess.PIPE).stdout.read())
-    elif sys.platform == 'win32':
-        percent_lost_re = re.compile('(?<percent_lost>[0-9]{1,3})% loss')
-        min_max_avg_re = re.compile('Minimum = (?P<min>\d+)ms, Maximum = (?P<max>\d+)ms, Average = (?P<avg>\d+)ms')
-
-        data = str(subprocess.Popen(['ping', host , '-n', str(count)], stdout=subprocess.PIPE).stdout.read())[2:-1]
-    else:
-        raise Exception('Unsupported Platform.')
-
-    percent_lost_match = percent_lost_re.search(data)
-    timing_match = min_max_avg_re.search(data)
-
-    percent_lost = int(round(float(percent_lost_match.group('percent_lost'))))
-    min_ms = int(round(float(timing_match.group('min'))))
-    average_ms = int(round(float(timing_match.group('avg'))))
-    max_ms = int(round(float(timing_match.group('max'))))
-
-    return percent_lost, min_ms, average_ms, max_ms
-
-# ------
-def dump_data_to_csv():
-    csv.register_dialect('pretty', delimiter='\t', quoting=csv.QUOTE_NONE)
-
-    with database_connection:
-        with open('pings.csv', 'w', newline='') as ping_file:
-            print('\t'.join('Date/time Percent_Lost Packets_Sent Min Max Avg'.split()), file=ping_file)
-
-            ping_writer = csv.writer(ping_file, dialect='pretty')
-            ping_data = database_connection.execute('SELECT * FROM pings').fetchall()
-            ping_writer.writerows(ping_data)
-
-        with open('speedtests.csv', 'w', newline='') as speedtest_file:
-            print('\t'.join('Date/time Ping Download Upload Server'.split()), file=speedtest_file)
-
-            speedtest_writer = csv.writer(speedtest_file, dialect='pretty')
-            speedtest_data = database_connection.execute('SELECT * FROM speedtests').fetchall()
-            speedtest_writer.writerows(speedtest_data)
-
-# ------
-def main():
-    maybe_create_tables()
-
-    consprint('Date/time        Lost   Min    Max    Avg    |    Ping   Download   Upload     Server')
-    while 1:
-        ping_speedtest_save()
-        time.sleep(DELAY_MINUTES * 60)
-
-# ------
 if __name__ == '__main__':
-    try:
-        main()
+    import argparse
 
+    parser = argparse.ArgumentParser(description='Check connection latency speed!',
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--ping-host', type=str, default='google.com', help='Hostname or ip to ping')
+    parser.add_argument('--db-filename', type=str, default='connection_date.sqlite', help='Database filename')
+    parser.add_argument('--ping-count', type=int, default=25, help='Number of pings to perform during each test')
+    parser.add_argument('--test-delay', type=int, default=10, help='Delay between each test, in minutes')
+    parser.add_argument('--timezone', type=str, default='US/Pacific', help='Timezone to use for log timestamps')
+    parser.add_argument('--timestamp-format', type=str, default='YY/MM/DD HH:mm', help='Timezone format for logs')
+
+    args = parser.parse_args()
+
+    pnc = PyNetCheck(ping_count=args.ping_count,
+                     ping_host=args.ping_host,
+                     test_delay=args.test_delay,
+                     db_filename=args.db_filename,
+                     timezone=args.timezone,
+                     timestamp_format=args.timestamp_format)
+
+    try:
+        pnc.run()
     except KeyboardInterrupt:
-        consprint('\nDumping most recent data to CSVs...')
-        dump_data_to_csv()
-        consprint()
+        pnc.consprint('\nDumping most recent data to CSVs...')
+        pnc.dump_data_to_csv()
+        pnc.consprint()
